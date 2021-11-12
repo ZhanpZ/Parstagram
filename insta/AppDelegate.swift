1cacdc9a8b64cc0fd508f5ea48f84c0d03f364f9
ZhanpZ <66219129+ZhanpZ@users.noreply.github.com>
2021-11-12 18:53:04 -0500

    11/12/2021

---
+++ insta/AppDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.swift
+//  insta
+//
+//  Created by Zhanping Zeng on 11/11/21.
+//
+
+import UIKit
+import Parse
+import MessageInputBar
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        // --- Copy this only
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "t6Ep4hih4oTxd6zFGHOx13XPQBhzkwIhMbU9ldyo" // <- UPDATE
+                $0.clientKey = "Sxo1L3mob2Td2yPyDvdTQrE4dJk4cyQ6L6peP8Ka" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        // --- end copy
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
